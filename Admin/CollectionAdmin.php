7fe39b929ea23ec9f0dad1962fbe35f023c96882
simon chrzanowski <simonch@gmx.de>
2016-05-31 11:25:16 +0200

    Update CollectionAdmin.php

--- Admin/CollectionAdmin.php
+++ Admin/CollectionAdmin.php
@@ -11,12 +11,12 @@
 
 namespace Sonata\ClassificationBundle\Admin;
 
-use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class CollectionAdmin extends Admin
+class CollectionAdmin extends AbstractAdmin
 {
     protected $formOptions = array(
         'cascade_validation' => true,
